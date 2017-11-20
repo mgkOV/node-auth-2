c51189a297cb5915b0907e84b391610deeff7642
mgkOV <myagkov.mww@gmail.com>
2017-11-20 18:20:03 +0300

    add cors

--- index.js
+++ index.js
@@ -4,23 +4,32 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 
-// DB setup
+// DB and CORS setup
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
+
 mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV === 'dev') {
   mongoose.connect('mongodb://localhost/auth_2', {
     useMongoClient: true
   });
 } else if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect('mongodb://localhost/auth_2_dev', {
+  mongoose.connect('mongodb://localhost/auth_2_production', {
     useMongoClient: true
   });
+  //CORS origin production setup
+  corsOptions.origin = 'http://example.com';
 }
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors(corsOptions));
 app.use(bodyParser.json({ type: '*/*' }));
 
 //routes
